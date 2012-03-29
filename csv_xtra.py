753333ad40e6d075d73e850ee1552988e31b3e75
Erik Taubeneck <erik.taubeneck@gmail.com>
2012-03-29 16:13:06 -0400

    First Commit

---
+++ csv_xtra.py
@@ -0,0 +1,74 @@
+import types
+import csv
+
+class tmp_class(object):
+    def __init__(self):
+        pass
+
+def export_data(data, attributes, fileName = 'dataOut.csv'):
+    dataOut = []
+    tmp = []
+    for a in attributes:
+        if type(a) == types.StringType:
+            tmp.append(a)
+        elif type(a) == types.TupleType:
+            tmp.append(a[0]+'.'+a[1])
+        else:
+            print type(a) 
+    dataOut.append(tmp)
+    for x in data:
+        tmp = []
+        for a in attributes:
+            if type(a) == types.StringType:
+                try:
+                    tmp.append(getattr(x,a))
+                except AttributeError:
+                    tmp.append('nan')
+            elif type(a) == types.TupleType:
+                tmp.append(getattr(getattr(x,a[0]),a[1]))
+            else:
+                print type(a)
+        dataOut.append(tmp)
+    f = open(fileName, 'wb')
+    csvWriter = csv.writer(f, delimiter = ',')
+    csvWriter.writerows(dataOut)
+    f.close()
+    
+def import_data(fileName, delimiter_in=','):
+    data = []    
+    f = open(fileName, 'rb')
+    csvReader = csv.reader(f,delimiter=delimiter_in)
+    header = csvReader.next()
+    for row in csvReader:
+        x = tmp_class()
+        for h in header:
+            tmp_value = row[header.index(h)]
+            try:
+                tmp_value = int(tmp_value)
+            except ValueError:
+                try:
+                    tmp_value = float(tmp_value)
+                except ValueError:
+                    pass
+            setattr(x,h,tmp_value)
+        data.append(x)
+    return(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='csv_xtra',
+      version='0.5',
+      py_modules=['csv_xtra'],
+      )
